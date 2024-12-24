3e8b06dbcc4abd5d2becaaa20db99c0bfb1ef623
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2024-12-24 16:33:23 +0900

    fix next-pwa -> @ducanh2912/next-pwa

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 import path from 'path';
 import { config as dotenvConfig } from 'dotenv';
 import { Configuration } from 'webpack';
-import nextPWA from 'next-pwa';
+import nextPWA from '@ducanh2912/next-pwa';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenvConfig({ path: path.resolve(process.cwd(), '../.env') });
@@ -10,9 +10,12 @@ if (process.env.NODE_ENV !== 'production') {
 
 const withPWA = nextPWA({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  workboxOptions: {
+    skipWaiting: true,
+    clientsClaim: true,
+  },
 });
 
 const nextConfig: NextConfig = withPWA({
